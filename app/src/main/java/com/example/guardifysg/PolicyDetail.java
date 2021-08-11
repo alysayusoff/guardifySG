87ade157e78e9b978e4d1c71d81f5193f89a34c7
Ooi Qi Ying <qiying1998@hotmail.com>
2021-08-11 09:56:00 +0800

    updated detailsPage with expandable text view and added latest news: greateastern_covid_plan onto landing page

---
+++ app/src/main/java/com/example/guardifysg/PolicyDetail.java
@@ -0,0 +1,46 @@
+package com.example.guardifysg;
+
+public class PolicyDetail {
+
+    private String detailTitle;
+    private String detailText;
+    private boolean expanded;
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+
+    public PolicyDetail(String detailTitle, String detailText) {
+        this.detailTitle = detailTitle;
+        this.detailText = detailText;
+        this.expanded = false;
+    }
+
+    public String getDetailTitle() {
+        return detailTitle;
+    }
+
+    public void setDetailTitle(String detailTitle) {
+        this.detailTitle = detailTitle;
+    }
+
+    public String getDetailText() {
+        return detailText;
+    }
+
+    public void getDetailText(String detailText) {
+        this.detailText = detailText;
+    }
+
+    public String toString(){
+        return "PolicyDetails{" +
+                "detailTitle='" + detailTitle + '\'' +
+                ", detailText='" + detailText +
+                '}';
+    }
+
+}
